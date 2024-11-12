e824037d60cc8f1daf8f28c58959358782ed8a13
a5dulaz1z <abdulaziznnn@gmail.com>
2024-11-12 18:45:19 +0600

    yu

--- 211.py
+++ 211.py
@@ -1,1 +1,23 @@
-print("dfghjklkjhgfdsfgh")
+from tkinter import *
+import random as rdm
+
+
+class Main(Frame):
+    def __init__(self, root):
+        super(Main, self).__init__(root)
+        self.startUI()
+
+    def startUI(self):
+        pass
+
+
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry("500x500+200+200")
+    root.title("Камень, ножницы, бумага")
+    root.resizable(False, False)
+    root["bg"] = "#FFF"
+    app = Main(root)
+    app.pack()
+    root.mainloop()
+
